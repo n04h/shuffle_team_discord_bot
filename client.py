4575a8cf2d00a0018c6c968da04a09a97e7fc2fe
N04h <contact@n04h.jp>
2020-12-31 01:42:53 +0900

    wip

--- client.py
+++ client.py
@@ -13,7 +13,26 @@ class MyClient(discord.Client):
 
     async def shuffle(self, message):
       msg = message.content.split()
-      await message.channel.send(msg[1])
+      vc = self.get_join_voice_channel(message)
+
+      if vc is None:
+        await message.channel.send('ボイスチャンネルを特定できませんでした。')
+      else:
+        await message.channel.send(vc.name)
+
+    def get_join_voice_channel(self, message):
+      # VCを全取得してジョインしてるチャンネルを探す
+      vcs = message.guild.voice_channels
+      for vc in vcs:
+        members = vc.members
+        for member in members:
+          if member.id == message.author.id:
+            return vc
+      return None
+
+    def get_channel_users(self, channel):
+      # チャンネルのユーザー一覧を返す
+      aa = 1
 
 client = MyClient()
 client.run(environ.TOKEN)
